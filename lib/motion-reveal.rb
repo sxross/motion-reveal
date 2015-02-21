c99667941e845bed76b197300c670751671b45d0
Steve Ross <sxross@gmail.com>
2015-02-21 11:19:10 -0800

    Updated to link in libz.dylib for Reveal 1.5

--- lib/motion-reveal.rb
+++ lib/motion-reveal.rb
@@ -5,7 +5,9 @@ end
 Motion::Project::App.setup do |app|
   app.development do
     if File.exist? ('/Applications/Reveal.app')
-      app.vendor_project('/Applications/Reveal.app/Contents/SharedSupport/iOS-Libraries/Reveal.framework', :static, :products => ['Reveal'], :cflags => '-ObjC')
+      app.libs += ['/usr/lib/libz.dylib', '/usr/lib/libc++.dylib']
+      app.vendor_project '/Applications/Reveal.app/Contents/SharedSupport/iOS-Libraries/Reveal.framework', :static, :products => ['Reveal'], :headers_dir => 'Headers'
+#      app.vendor_project('/Applications/Reveal.app/Contents/SharedSupport/iOS-Libraries/Reveal.framework', :static, :products => ['Reveal'], :cflags => '-ObjC -lz')
       app.frameworks << 'CFNetwork'
       app.frameworks << 'QuartzCore'
 
